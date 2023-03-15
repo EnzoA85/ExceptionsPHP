679d466360c60c33dcc66a12db70b220c626bc20
Enzo ARCHAMBAUD <bib.enzoarchambaud@gmail.com>
2023-03-15 09:05:39 +0100

    add H1 'La Poste'

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ class Gaston
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Gaston</title>
 </head>
+<h1>La Poste</h1>
 <body>
     <div class="alert alert-warning">
         <?php
