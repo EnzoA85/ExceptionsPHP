02e24515b61994615ed6890d6f806ac3cb60e65d
Enzo ARCHAMBAUD <bib.enzoarchambaud@gmail.com>
2023-03-15 09:37:50 +0100

    fix try catch

--- index.php
+++ index.php
@@ -22,9 +22,9 @@ class Gaston
         try {
             echo "<strong> Gaston </strong> - Encore ces contrats ? Ok, je les imprime... <br/>";
             $this->ImprimerContrats();
-            echo "A présent une petite signature...";
+            echo "<strong> Gaston </strong> - A présent une petite signature...<br/>";
             $this->AjouterSignature();
-            echo "Fantasio, les contrats sont signés !";
+            echo "<strong> Gaston </strong> - Fantasio, les contrats sont signés !<br/>";
         } catch (Exception $e) {
             echo"<div class = 'alert alert-danger'><strong> Gaston </strong> -  M'enfin ! ".$e->getMessage()."</div>";
         }
@@ -32,59 +32,75 @@ class Gaston
 
     function AjouterSignature() 
     {
-        echo"Signez ici , M'sieur Demesmaeker.";
+        echo"<strong> Gaston </strong> - Signez ici , M'sieur Demesmaeker.<br/>";
     }
 
     function ImprimerContrats()
     {
         echo "<strong> Gaston </strong> - D'abord, mettre en route l'imprimante.<br/>";
         $this->AllumerImprimante();
-        echo"Voilà, c'est fait !";
+        echo" Voilà, c'est fait ! <br/>";
     }
 
     function AllumerImprimante()
     {
         echo"<strong> Gaston </strong> - Voyons comment allumer cette machine...";
-        throw new Exception("Mais qui a démonté tout l'intérieur ?");
+        //throw new Exception("Mais qui a démonté tout l'intérieur ?");
     }
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>Gaston</title>
-</head>
-<img src="https://upload.wikimedia.org/wikipedia/fr/2/2a/Logo-laposte.png" width="10%">
-<body>
-    <hr color="black" style="margin-top: 50px;">
-    <h3>Trier le courrier en retard</h3><br/>
-    <div class="alert alert-warning">
-        <?php
-            echo " <strong> Chef </strong> - Debout Gaston ! Il faut trier le courrier ! <br/>";
-        ?>
-    </div>
-    <?php
-        $gaston = new Gaston();
-        $gaston->TrierCourrierEnRetard(3);
-    ?>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <title>Gaston</title>
+    </head>
+    <img src="https://upload.wikimedia.org/wikipedia/fr/2/2a/Logo-laposte.png" width="10%">
+    <body>
+        <hr>
+        <div class="trierCourrier">
+            <h3>Trier le courrier en retard</h3><br/>
+            <div class="alert alert-warning">
+                <?php
+                    echo " <strong> Chef </strong> - Debout Gaston ! Il faut trier le courrier ! <br/>";
+                ?>
+            </div>
+            <?php
+                $gaston = new Gaston();
+                $gaston->TrierCourrierEnRetard(3);
+            ?>
+        </div>
 
-    <hr color="black" style="margin-top: 50px;">
+        <hr style="margin-top: 50px;">
 
-    <h3>Imprimer les contrats</h3><br/>
-    <div class = "alert alert-warning">
-        <?php
-            echo"<strong> Chef </strong> - Gaston, Mr.Demesmaeker arrive, faites vite!";
-        ?>
-    </div>
-    <?php
-        $gaston = new Gaston();
-        $gaston->FaireSignerContrats();
-    ?>
-    <hr color="black" style="margin-top: 50px;margin-bottom: 50px;">
-</body>
+        <div class="imprimerContrats">
+            <h3>Imprimer les contrats</h3><br/>
+            <div class = "alert alert-warning">
+                <?php
+                    echo"<strong> Chef </strong> - Gaston, Mr.Demesmaeker arrive, faites vite!";
+                ?>
+            </div>
+            <?php
+                $gaston = new Gaston();
+                $gaston->FaireSignerContrats();
+            ?>
+        </div>
+        
+        <hr style="margin-top: 50px;margin-bottom: 50px;">
+    </body>
 </html>
+
+<style>
+    .trierCourrier {
+        margin-left: 50px;
+        margin-right: 50px;
+    }
+    .imprimerContrats {
+        margin-left: 50px;
+        margin-right: 50px;
+    }
+</style>
